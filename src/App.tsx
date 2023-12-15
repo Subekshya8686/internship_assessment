eae67ffce7e293201dbbea1361eebea7e5e05fc4
Subekshya8686 <subekshyakayastha4@gmail.com>
2023-12-15 16:26:27 +0545

    login and product list page

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,6 @@ import {createBrowserRouter, RouterProvider} from "react-router-dom";
 import './App.css'
 import Login from "./pages/Login.tsx";
 import ListPage from "./pages/ListPage.tsx";
-// import ReactDOM from "react-dom/client";
-// import React from "react";
-// import User_Login from "./pages/new.tsx";
-
 
 const router=createBrowserRouter(
     [
--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -25,7 +25,6 @@ function Login() {
                         Login
                     </button>
                 </Link>
-                {/*</form>*/}
             </div>
         </div>
         </>
